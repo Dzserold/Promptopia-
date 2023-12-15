5d4031be7c4ecf3c2e3e7a43c995376fc9b806ee
F_Dzserold <f.dzserold@gmail.com>
2023-12-15 16:49:47 +0100

    set google auth and mongodb

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,46 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import User from "@models/user";
+import { connectToDb } from "@utils/database";
 
 const handler = NextAuth({
-  providers: [GoogleProvider({ clientId: "", clientSecret: "" })],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  // checking whick user logged in with email
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+
+  //Function creates new user if it doesn't exist yet
+  async signIn({ profile }) {
+    try {
+      await connectToDb();
+      // Check if the user already exist
+      const userExist = await User.findOne({
+        email: profile.email,
+      });
+      if (!userExist) {
+        await User.create({
+          email: profile.email,
+          username: profile.username.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
