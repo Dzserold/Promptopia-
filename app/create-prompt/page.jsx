03b637ee073a9c53e9ab4cb0d7dc027aefa13209
F_Dzserold <f.dzserold@gmail.com>
2023-12-17 22:39:39 +0100

    completing create prompt functionality

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -6,13 +6,38 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 function CreatePrompt() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [subbmiting, setSubbmiting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (e) => {};
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubbmiting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubbmiting(false);
+    }
+  };
 
   return (
     <Form
